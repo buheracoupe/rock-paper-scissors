ec75047a840bf66da877feb5eff5de7fab850e61
buheracoupe <darrelltduri@gmail.com>
2024-09-30 23:12:12 +0200

    add computerPick logic and retrieve user pick Option

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const rulesModal = document.querySelector(".rules-modal");
 const rulesBtn = document.querySelector("#rules");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const optionsArray = [rock, paper, scissors];
+const userOptions = document.querySelectorAll(".hand")
+let selectedOption;
+
+// generate random Computer Pick
+function randomPick(){
+    randomOptionIndex = Math.round(Math.random()*2)
+    randomOption = optionsArray[randomOptionIndex]
+    console.log(randomOption)
+    return randomOption
+}
+const computerChoice = randomPick();
+
+userOptions.forEach((option) => {
+    option.addEventListener("click", option => selectedOption = option)
+})
 
 
 
